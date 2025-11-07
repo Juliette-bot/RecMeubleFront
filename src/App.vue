afd2cd639d9cd2639785102209c03165fa7910db
juliette <jblanc17@outlook.fr>
2025-11-07 15:41:47 +0100

    try some compo

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import DisplayFurniture from './compo/DisplayFurniture.vue'
-import FooterCompo from './compo/FooterCompo.vue'
-import NavBar from './compo/NavBar.vue'
+import DisplayFurniture from './components/DisplayFurniture.vue'
+import FooterCompo from './components/FooterCompo.vue'
+import NavBar from './components/NavBar.vue'
 import LoginViews from './views/loginViews.vue'
 </script>
 
